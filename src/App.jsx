c58abee178d3e160e13564f1d553ac3adff9c622
Eriko Syah <erikosyahputraf@gmail.com>
2024-12-17 15:18:47 +0700

    Add: community section and motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Services from "./components/Services/Services";
 import Banner from "./components/Banner/Banner";
 import Subscribe from "./components/Subscribe/Subscribe";
+import Banner2 from "./components/Banner/Banner2";
 const App = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark ">
@@ -11,6 +11,7 @@ const App = () => {
       <Services />
       <Banner />
       <Subscribe />
+      <Banner2/>
     </main>
   );
 };
