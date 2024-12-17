f34ea1e0cdf4737173d779e1af81397c6d3d2ca2
Eriko Syah <erikosyahputraf@gmail.com>
2024-12-17 15:04:00 +0700

    Add: Subscribe section with motion

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Services from "./components/Services/Services";
 import Banner from "./components/Banner/Banner";
+import Subscribe from "./components/Subscribe/Subscribe";
 const App = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark ">
       <Hero />
       <Services />
       <Banner />
+      <Subscribe />
     </main>
   );
 };
